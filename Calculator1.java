a3fba3207adf0275f4dd09f0cfa315d28d3f57ef
Bhumi2021 <abpatel20817@gmail.com>
2021-03-15 22:12:53 +0000

    First java programme completed

---
+++ Calculator1.java
@@ -0,0 +1,13 @@
+public class Calculator1 {
+    static int a = 30;
+    static int b = 10;
+    static int total = 40;
+
+    public static void main(String[] args){
+        System.out.println("Addition of "+ a + " & "+ b +" = "+total );
+        System.out.println( "Subtraction of "+ a + " & "+ b +" = " + (a - b));
+        System.out.println("Multiplication of "+ a + " & "+ b +" = " +(a * b));
+        System.out.println("Division of "+ a + " & "+ b +" = " +(a/b));
+
+    }
+}
---
+++ PersonalDetails.java
@@ -0,0 +1,20 @@
+public class PersonalDetails {
+    public static void main(String[] args){
+        String Name = "Bhumi Patel";
+        String Address = "31 Belgrave road ";
+        String MobileNumber = "07904653928";
+        int Age = 35;
+        float SecurityCode = 35.60f;
+        boolean Eligibleforvote = true;
+        char Grade = 'b';
+        System.out.println( "Name:" + Name);
+        System.out.println( "Address:" +Address);
+        System.out.println( "Mobile Number:"+MobileNumber);
+        System.out.println( "Age:"+Age);
+        System.out.println("Security code:"+SecurityCode);
+        System.out.println("Eligible for vote:"+Eligibleforvote);
+        System.out.println("Grade:"+Grade);
+
+
+    }
+}
